3948e3e4f2a628b757794ba234c0771cb081f4d0
Vickmaur <Vickmaur@gmail.com>
2022-09-08 21:31:47 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h> 
   
-/** 
+ /** 
   * main - Entry point 
   * 
   * Return: Always 0 (Success) 
@@ -19,4 +19,4 @@
          printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c)); 
          printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f)); 
          return (0); 
- }			
+ }
