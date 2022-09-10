267e3dd1da16750958c25869baa5527be553542d
Vickmaur <Vickmaur@gmail.com>
2022-09-10 04:09:24 +0100

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,10 +7,10 @@
    * Return: Always (Success)
    */
  int main(void)
- {
-         int c, i;
+{
+ int c, i;
 
-         for (c = '0'; c <= '9'; c++)
+        for (c = '0'; c <= '9'; c++)
          {
                  for (i = '0'; i <= '9'; i++)
                  {
