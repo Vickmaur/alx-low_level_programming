fa7ffe87d8bb48b0aca44e7086d44599f7125b64
Vickmaur <Vickmaur@gmail.com>
2022-09-13 17:01:03 +0100

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * _islower a function that checks for lower case characters
+ * _islower a function that checks for lower 
+ * case characters.
  * @c: single letter input
- * Return: 1 if int c is lowercase, 0 if otherwise
+ * Return: 1 if int c is lowercase, 0 if
+ * otherwise return 0
  */
 int _islower(int c)
 {
