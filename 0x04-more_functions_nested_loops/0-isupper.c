7eb6aac3758969a0a7abf086704100195b80845d
Vickmaur <Vickmaur@gmail.com>
2022-09-15 10:39:50 +0100

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is upperca * se
+ * @c: carriage integer variable
+ * Return: 1 of true, 0 of false
+ */
+int _isupper(int c)
+{
+	if (c >= 65, && c <= 90)
+		{
+			return (1);
+		}
+	return (0);
+}
