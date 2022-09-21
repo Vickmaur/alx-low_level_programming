29257d5cdff42529e0f7a04383a6bd4e7c2f680b
Vickmaur <Vickmaur@gmail.com>
2022-09-21 13:52:31 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,9 +2,9 @@
 
 /**
  * _strcmp - Compares pointers to two strings
- * @s1: A pointer to the first string to be co * mpared
- * @s2: A pointer to the second string to be c * ompared
- * Return: If str1 < str2, the negative differ * ence of the first 2 unmatched strings
+ * @s1: A pointer to the first string
+ * @s2: A pointer to the second string to be
+ * Return: If str1 < str2, the negative
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -13,5 +13,5 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-	return(*s1 - *s2);
+	return (*s1 - *s2);
 }
