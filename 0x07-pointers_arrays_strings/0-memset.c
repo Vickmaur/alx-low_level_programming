685f367b2059b7d165c514f8804e11a0075cdf56
Vickmaur <Vickmaur@gmail.com>
2022-09-26 17:32:21 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,45 +1,20 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * *_memset - fills memory with a constant byt * e.
+ * @s: pointer to the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
  */
-int main(void)
+char *_memset(char *s, char b, unsigned int n)
 {
-    char buffer[98] = {0};
-    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+	unsigned int i;
 
-    simple_print_buffer(buffer, 98);
-    _memcpy(buffer + 50, buffer2, 10);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);
-    return (0);
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
