97b1282dc83b41adbee90925f15ffbee46bf6c51
Vickmaur <Vickmaur@gmail.com>
2022-09-28 19:40:10 +0100

    print recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in r * everse
+ * @s: String to be printed
+ */
+void _print_rev_recursion(char *s);
+{
+	if (*s)
+	{
+		_print_rev_recursion(char *s) (s + 1);
+		_putchar(*s);
+	}
+}
