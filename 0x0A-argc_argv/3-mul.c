fc41c636babb0e7e0c3a65e89a811738f1d9af89
Vickmaur <Vickmaur@gmail.com>
2022-10-02 09:40:44 +0100

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Prints the products of two given int * egers
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1,error Otherwise 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+			j *= atoi(argv[i]);
+
+		printf("d\n", j);
+	}
+
+	return (0);
+}
