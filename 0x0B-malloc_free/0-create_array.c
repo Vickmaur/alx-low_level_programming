cf3704d47bb371c442c3189147b204309d8d3995
Vickmaur <Vickmaur@gmail.com>
2022-10-05 11:35:23 +0100

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - Creates an array of chars an * d initializes it with a specific char.
+ * create_array - Creates an array of characte * rs.
  * @size: The size of the array to be initiali * zed.
  * @c: The specific char to intialize the arra * y with.
  * Return: If size == 0 or the function fails  * - NULL
