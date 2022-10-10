b4a56e381338a39edf1a015afbb4a04df3c181cf
Vickmaur <Vickmaur@gmail.com>
2022-10-10 10:26:01 +0100

    0x0E. C - Structures, typedef project

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,7 @@ struct dog
 typedef struct dog dog_t;
 
 int _putchar(char c);
-void init_dog(struct dog *d, char *name, floatage, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
