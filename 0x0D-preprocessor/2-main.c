4b60a6aea0c53030daf341a19840792beb36b4c4
Vickmaur <Vickmaur@gmail.com>
2022-10-10 17:47:39 +0100

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the name of the file bein * compiled
+ * @void: Nothing
+ * Return: Success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE,__);
+
+	return (0);
+}
